e35b822ca91881d8d49e4d7284203cc95529b2db
Roberto Andres Ruiz Pereira <rpruiz33@gmail.com>
2025-03-29 15:55:27 -0300

    awsdsdfd

--- src/components/hardCard.jsx
+++ src/components/hardCard.jsx
@@ -1,19 +1,19 @@
 import React from 'react';
 import './hardCard.css';
 
-const HardCard = ({ index, items, item ,extraInfo,periodo }) => {
+const HardCard = ({ index, items, item, extraInfo, periodo }) => {
   return (
-    <div className="cards-container ">
+    <div className="cards-container">
       <div className="card border-success mb-3 bg-custom" style={{ maxWidth: '18rem' }}>
-        <div className="card-body text-success color-black">
+        <div className="card-body text-success">
           <h5 className="card-title c-black">
-            <div className="c-b" key={index}>
-              {{items,periodo}}
+            <div className="c-b">
+              {items} - {periodo} {/* Corrige la forma de mostrar los datos */}
             </div>
           </h5>
         </div>
         <div className="card-footer bg-w m-7">
-          <div key={index}>{item}{extraInfo}</div>
+          <div>{item} {extraInfo}</div>
         </div>
       </div>
     </div>
